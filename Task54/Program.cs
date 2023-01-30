180e91ba994f719b7d13c6612d878c9beae1e004
vlad <vladislavludinov3006@gmail.com>
2023-01-30 19:34:52 +0300

    method sorting array added

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,27 +1,47 @@
 ﻿using static System.Console;
 
-int [,] getArray()
+int[,] getArray()
 {
     Random rnd = new Random();
-    int [,] arr = new int [rnd.Next(3,7),rnd.Next(3,7)];
+    int[,] arr = new int[rnd.Next(3, 7), rnd.Next(3, 7)];
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            arr[i,j] = rnd.Next(1,11);
+            arr[i, j] = rnd.Next(1, 11);
         }
     }
     return arr;
 }
 
-void printArray(int [,] arr)
+void printArray(int[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Write(arr[i,j]);
+            Write(arr[i, j]);
         }
         WriteLine("");
+    }
+}
+
+void cortArray(int[,] arr)
+{
+    int temp = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = j; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, j] < arr[i, k + 1])
+                {
+                    temp = arr[i, j];
+                    arr[i, j] = arr[i, k + 1];
+                    arr[i, k + 1] = temp;
+                }
+            }
+        }
     }
 }
