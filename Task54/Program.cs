f8322640db8ed4e3cb5a788ccbf015c3b96313ef
vlad <vladislavludinov3006@gmail.com>
2023-01-30 19:09:42 +0300

    method creating array added

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int [,] getArray()
+{
+    Random rnd = new Random();
+    int [,] array = new int [rnd.Next(3,7),rnd.Next(3,7)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(1,11);
+        }
+    }
+    return array;
+}
+
