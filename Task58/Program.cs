9215705ab837656ca9437e37f7dded4784068f02
vlad <vladislavludinov3006@gmail.com>
2023-01-31 18:06:04 +0300

    method multiplication matrixs added

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -25,3 +25,41 @@ void printMatrix(int [,] matrix)
         WriteLine("");
     }
 }
+
+int [,] multiplyMatrix(int [,] matrixA, int [,] matrixB)
+{
+    if (matrixA.GetLength(1) == matrixB.GetLength(0))
+    {
+        int [,] matrixC = new int [matrixA.GetLength(0), matrixB.GetLength(1)];
+        for (int i = 0; i < matrixC.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixC.GetLength(1); j++)
+            {
+                matrixC[i,j] = 0;
+                for (int k = 0; k < matrixA.GetLength(1); k++)
+                {
+                    matrixC[i,j] += matrixA[i,k] * matrixB[k,j];
+                }
+            }
+        }
+    }
+    else
+    {
+        if (matrixB.GetLength(1) == matrixA.GetLength(0))
+        {
+            int [,] matrixC = new int [matrixB.GetLength(0), matrixA.GetLength(1)];
+            for (int i = 0; i < matrixC.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixC.GetLength(1); j++)
+                {
+                    matrixC[i,j] = 0;
+                    for (int k = 0; k < matrixB.GetLength(1); k++)
+                    {
+                        matrixC[i,j] += matrixB[i,k] * matrixA[k,j];
+                    }
+                }
+            }
+        }
+    }
+    return matrixC;
+}
