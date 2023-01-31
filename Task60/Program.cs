2affb850d2a059e41235aa5514066d6f7997e3c9
vlad <vladislavludinov3006@gmail.com>
2023-01-31 19:27:04 +0300

    method generate non repeating matrix added

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int [,] getMatrix()
+{
+    Random rnd = new Random();
+    int [,] matrix = new int [rnd.Next(2,5), rnd.Next(2,5), rnd.Next(2,5)];
+    int [] check = new int [matrix.GetLength(0)*matrix.GetLength(1)*matrix.GetLength(2)];
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                while (indicator)
+                {
+                    matrix[i,j,k] = rnd.Next(10,100);
+                    for (int n = 0; n < check.Length; n++)
+                    {
+                        if (matrix[i,j,k] != check[n])
+                        {
+                            temp +=1;
+                        }
+                    }
+                    if (temp == check.Length)
+                    {
+                        indicator = false;
+                    }
+                    temp = 0;
+                }
+                indicator = true;
+            }
+        }
+    }
+    return matrix;
+}
