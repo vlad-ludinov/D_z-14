f54b9cfb10ecb4e6390e1b1b6489ea5674eb0551
vlad <vladislavludinov3006@gmail.com>
2023-01-31 17:43:44 +0300

    method created matrix added

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int [,] getMatrix()
+{
+    Random rnd = new Random();
+    int [,] matrix = new int [rnd.Next(2,5), rnd.Next(2,5)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1,11);
+        }
+    }
+    return matrix;
+}
+
